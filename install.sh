1c7bd32072a3bcc354e42b198a75b6d349357e73
NinjaTronX <ninjatronx@proton.me>
2025-07-11 22:42:23 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@
 python3 -m venv venv
 # Activate the virtual environment
 source venv/bin/activate
-
+l
+# Install manually if this download stalls out. The url to download it manually will be in the output
 pip install torch==2.1.1+cu118 torchaudio==2.1.1+cu118 --index-url https://download.pytorch.org/whl/cu118
 
 # Install other dependencies from requirements.txt
